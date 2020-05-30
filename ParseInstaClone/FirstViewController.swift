122ea8627c0a9e2b2973e2f7889b50fba7c67068
sefacelik <sefacelik1800@gmail.com>
2020-05-30 23:23:55 +0300

    Initial Commit

---
+++ ParseInstaClone/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ParseInstaClone
+//
+//  Created by Sefa Çelik on 30.05.2020.
+//  Copyright © 2020 Sefa Celik. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ParseInstaClone/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ParseInstaClone
+//
+//  Created by Sefa Çelik on 30.05.2020.
+//  Copyright © 2020 Sefa Celik. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
